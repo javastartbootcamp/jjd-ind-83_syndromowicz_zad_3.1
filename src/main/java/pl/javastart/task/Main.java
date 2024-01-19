66c8cfc141ca4f36f33aaf696555f3e3edbb9ecb
syndromowicz <kleofasx@gmail.com>
2024-01-19 16:11:29 +0100

    solution

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -3,6 +3,15 @@ package pl.javastart.task;
 public class Main {
 
     public static void main(String[] args) {
-
+        Tv tv1 = new Tv();
+        tv1.displayOn = true;
+        tv1.displaySize = 65.0;
+        tv1.tvProducer = "samsung";
+        Tv tv2 = new Tv();
+        tv2.displayOn = false;
+        tv2.displaySize = 55.0;
+        tv2.tvProducer = "LG";
+        System.out.println(tv1.tvProducer + " " + tv1.displaySize + " " + "ekran włączony " + tv1.displayOn);
+        System.out.println(tv2.tvProducer + " " + tv2.displaySize + " " + "ekran włączony " + tv2.displayOn);
     }
 }
---
+++ src/main/java/pl/javastart/task/Tv.java
@@ -0,0 +1,10 @@
+package pl.javastart.task;
+//nie wiem czemu tworzac nowa klase tutaj automatycznie dodaja sie te pakiety i nic recznie nie wpisuje
+//wiec pewnie dlatego w gicie widac ze to ja to tworze
+public class Tv {
+    double displaySize;
+    boolean displayOn;
+    String tvProducer;
+    Tv() {
+    }
+}
