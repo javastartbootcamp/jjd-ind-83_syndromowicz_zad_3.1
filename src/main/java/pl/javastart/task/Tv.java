d118bf09e3b31dae946c3523227474d905f0542e
syndromowicz <kleofasx@gmail.com>
2024-01-20 21:04:53 +0100

    solution

--- src/main/java/pl/javastart/task/Tv.java
+++ src/main/java/pl/javastart/task/Tv.java
@@ -1,15 +1,16 @@
 package pl.javastart.task;
-//nie wiem czemu tworzac nowa klase tutaj automatycznie dodaja sie te pakiety i nic recznie nie wpisuje
-//wiec pewnie dlatego w gicie widac ze to ja to tworze
+
 public class Tv {
     double displaySize;
     boolean displayOn;
     String tvProducer;
-    Tv(double size,boolean power, String prd) {
+
+    Tv(double size, boolean power, String prd) {
         displaySize = size;
         displayOn = power;
         tvProducer = prd;
     }
+
     void printInfo() {
         System.out.println("Ekran włączony: " + displayOn + " " + tvProducer + " " + displaySize);
     }
